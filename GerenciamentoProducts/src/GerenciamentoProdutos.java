73df4a79739a5b04643aab291f588b2ecaa9388d
Rafhael <legendar458@gmail.com>
2026-04-02 11:08:14 -0300

    feat: Novos médotos: Cadastrar e Listar.

--- GerenciamentoProducts/src/GerenciamentoProdutos.java
+++ GerenciamentoProducts/src/GerenciamentoProdutos.java
@@ -1,7 +1,34 @@
-public class GerenciamentoProdutos {
-    String nome;
-    double preco;
-    int quantidade;
-    
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class GerenciamentoProdutos {
+    ArrayList <String> listaProduto = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    String nomeProduto;
+    double preco;
+
+    public void CadastrarProduto(int quantidade) {
+        try{
+             for (int i = 0; i < quantidade; i++) {
+                 System.out.printf("Informe o %d° produto:", i+1);
+                    nomeProduto = sc.nextLine();
+                    listaProduto.add(nomeProduto);
+                 System.out.println("Produto Cadastrado!");
+                }
+        }catch(Exception error){
+            System.out.println("Produto não cadastrado, tente novamente!");
+            }
+    }
+
+    public void ListarProdutos(){
+
+        try {
+            for (String listar : listaProduto) {
+                System.out.println(listar);
+            }
+            System.out.println("Produtos listados com sucesso!");
+        }catch (Exception error){
+            System.out.println("Erro ao listar, tente novamente!");
+            }
+        }
 }
--- GerenciamentoProducts/src/Main.java
+++ GerenciamentoProducts/src/Main.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        GerenciamentoProdutos gProdutos = new GerenciamentoProdutos();
+        Scanner sc = new Scanner(System.in);
+        int quantidade;
+        int opcaoUsuario;
+        boolean continuarRodando = true;
+
+        System.out.println("Bem vindo ao gerenciamento de proudutos!");
+do {
+    System.out.println("Escolha uma dessas opções abaixo:");
+    System.out.println("1- Cadastrar produto.");
+    System.out.println("2- Listar produto.");
+    System.out.println("0- Encerrar programa.");
+    opcaoUsuario = sc.nextInt();
+
+    switch (opcaoUsuario) {
+        case 1:
+            System.out.println("Informe a quantidade de produtos que deseja incluir.");
+            quantidade = sc.nextInt();
+            gProdutos.CadastrarProduto(quantidade);
+            break;
+        case 2:
+            gProdutos.ListarProdutos();
+            break;
+        case 0:
+            continuarRodando = false;
+            break;
+        default:
+            System.out.println("Opção incorreta!");
+    }
+}while(continuarRodando);
+
+    }
 }
